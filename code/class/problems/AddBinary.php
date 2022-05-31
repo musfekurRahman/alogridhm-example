ae9eaba81211e14ab3734e98b2bfeb16fac2bd73
musfek <sajibma@gmail.com>
2022-05-31 12:13:58 +0600

    add two binary

--- code/class/problems/AddBinary.php
+++ code/class/problems/AddBinary.php
@@ -24,32 +24,20 @@ class AddBinary implements ClassInterface
         for ($i = 0; $i <= $n; $i++) {
             $a1 = $count_a >= 0 ? $a[$count_a] : 0;
             $b1 = $count_b >= 0 ? $b[$count_b] : 0;
-            $step=2;
             if (($a1 + $b1 + $carry) > 1) {
-                if(($a1 + $b1 + $carry) ==3){
-                    $sum[$i] =  1;
-                }else{
-                    $sum[$i] =  0;
-                }
+                $sum[$i] =(($a1 + $b1 + $carry) ==3) ? 1 : 0;
                 $carry = 1;
-                $step = 1;
             } else {
-
                 $sum[$i] = $a1 + $b1+$carry;
-
                 $carry = 0;
             }
-
             $count_a--;
             $count_b--;
         }
-
         $sum = array_reverse($sum);
-
         if ($carry > 0) {
             array_unshift($sum, 1);
         }
-
         return implode($sum);
 
     }
